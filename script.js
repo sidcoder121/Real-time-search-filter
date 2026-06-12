534ec5278cdde0aeaea6e005066a96044c806174
Siddharth Saxena <sidcoder73@gmail.com>
2026-06-12 23:52:22 +0530

    all file added

---
+++ script.js
@@ -0,0 +1,27 @@
+const users = [
+{
+name: "amisha rathore",
+pic: "https://i.pinimg.com/736x/cd/9b/1c/cd9b1cf5b96e8300751f952488d6c002.jpg", 
+bio: "silent chaos in a loud world |I not for everyone",
+},
+{
+name: "kiara mehta",
+pic: "https://i.pinimg.com/736x/1f/2f/85/1f2f856bf3a020ed8ee9ecb3306ae074.jpg",
+bio: "main character energy | coffee > everything +",
+},
+{
+name: "isha oberoi",
+pic: "https://i.pinimg.com/736x/23/48/7e/23487ef1268cfe017047a0640318c0d0.jpg",
+bio: "walking through dreams in doc martens | late night thinker",
+},
+{
+name: "Ojin Oklawa",
+pic: "https://i.pinimg.com/736x/01/be/94/01be94b0b5bf03a50b5d6c4bfec78063.jpg",
+bio: "too glam to give a damn | filter free soul",
+},
+{
+name: "Saanvi Kapoor",
+pic: "https://i.pinimg.com/736x/1f/2f/85/1f2f856bf3a020ed8ee9ecb3306ae074.jpg",
+bio: "chasing sunsets and dreams | unapologetically me",
+}
+];
